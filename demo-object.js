4f1d0b4980343c3c6e8d98ec760f98447f71353a
David Andersson <david.andersson@zocom.se>
2023-01-23 14:00:18 +0100

    Object example

---
+++ demo-object.js
@@ -0,0 +1,33 @@
+// Utan objekt - massa variabler
+let shoeStoreOwner = 'Sune Sandal'
+let shoeStoreAddress = 'Skogatan 7'
+
+
+// Skapa ett objekt med egenskaper och en metod
+// property och method
+let shoeStore = {
+	owner: 'Sune Sandal',
+	address: 'Skogatan 7',
+	city: 'Småland',
+	print() {
+		// "this" sätts till "shoeStore"
+		// console.log(shoeStore.owner, shoeStore.address)  <- fungerar men man gör bara inte så
+		console.log(this.owner, this.address)
+		console.log( this.obj.x )
+		// console.log(owner, address)  <- error
+	},
+	obj: { x: 15, y: 20, price: 50 }
+}
+
+let icecreamStore = {
+	owner: 'Gurre Glass',
+	shout() {
+		console.log('Kom och ät glass! Bara ' + shoeStore.obj.price + 'kr!')
+	}
+}
+
+// console.log( 'shoeStore: ', shoeStore )
+
+shoeStore.print()
+icecreamStore.shout()
+
